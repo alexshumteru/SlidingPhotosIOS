e428b7d51f3384f5e6c6c2f3fb4885feb1c0dfd5
Alex Shum <alexshum939@gmail.com>
2022-03-25 12:16:04 +0800

    Moved the UIImage extension code to another file

--- SlidingPhotos/ImageHandler.swift
+++ SlidingPhotos/ImageHandler.swift
@@ -44,3 +44,25 @@ class ImageHandler: NSObject  {
     }
 
 }
+
+extension UIImage {
+    func resizeImage(targetHeight: CGFloat, targetWidth: CGFloat) -> UIImage {
+        // Get current image size
+        let size = self.size
+
+        // Compute scaled, new size
+        let heightRatio = targetHeight / size.height
+        let widthRatio = targetWidth / size.width
+        let newSize = CGSize(width: size.width * widthRatio, height: size.height * heightRatio)
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+        // Create new image
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        self.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        // Return new image
+        return newImage!
+    }
+}
